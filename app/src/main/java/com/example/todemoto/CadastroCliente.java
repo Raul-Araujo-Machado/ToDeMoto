acbbecdf2c16ed02a710df086a448ab2912ca6c8
Raul Araujo Machado <raularaujomachado@gmail.com>
2022-06-13 10:19:51 -0300

    Testando a PrincipalActivity

--- app/src/main/java/com/example/todemoto/CadastroCliente.java
+++ app/src/main/java/com/example/todemoto/CadastroCliente.java
@@ -2,7 +2,9 @@ package com.example.todemoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroCliente extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CadastroCliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_cliente);
+    }
+
+    public void chamarPrincipal(View view){
+        Intent intent = new Intent(this, PrincipalActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/todemoto/CadastroMotociclista.java
+++ app/src/main/java/com/example/todemoto/CadastroMotociclista.java
@@ -2,7 +2,9 @@ package com.example.todemoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadastroMotociclista extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CadastroMotociclista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_motociclista);
+    }
+
+    public void chamarPrincipal(View view){
+        Intent intent = new Intent(this, PrincipalActivity.class);
+        startActivity(intent);
     }
 }
