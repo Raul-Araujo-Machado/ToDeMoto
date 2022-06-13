a0979bb95acaf05b2fc13310b393ab39e951991e
Raul Araujo Machado <raularaujomachado@gmail.com>
2022-06-13 11:14:57 -0300

    Add PerfilClienteActivity

--- app/src/main/java/com/example/todemoto/PrincipalActivity.java
+++ app/src/main/java/com/example/todemoto/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package com.example.todemoto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -61,5 +62,10 @@ public class PrincipalActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void chamarPerfil(View view){
+        Intent intent = new Intent(getApplicationContext(), PerfilClienteActivity.class);
+        startActivity(intent);
     }
 }
