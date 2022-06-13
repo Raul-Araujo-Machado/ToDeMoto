c809f381a278ecbab956873fb40e6d168ad02853
Raul Araujo Machado <raularaujomachado@gmail.com>
2022-06-13 13:45:04 -0300

    Atualização Perfil e Edição

--- app/src/main/java/com/example/todemoto/PerfilClienteActivity.java
+++ app/src/main/java/com/example/todemoto/PerfilClienteActivity.java
@@ -2,7 +2,9 @@ package com.example.todemoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerfilClienteActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class PerfilClienteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_cliente);
+    }
+    public void chamarEditarCliente(View view){
+        Intent intent = new Intent(this, EdicaoClienteActivity.class);
+        startActivity(intent);
     }
 }
