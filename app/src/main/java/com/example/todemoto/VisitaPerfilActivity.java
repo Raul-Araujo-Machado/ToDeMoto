0d473372de557dc78be771ce32ad6bd69cf1ee0e
Raul Araujo Machado <raularaujomachado@gmail.com>
2022-07-10 10:40:17 -0300

    Add VisitaMotociclistaActivity

---
+++ app/src/main/java/com/example/todemoto/VisitaPerfilActivity.java
@@ -0,0 +1,14 @@
+package com.example.todemoto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitaPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visita_perfil);
+    }
+}
